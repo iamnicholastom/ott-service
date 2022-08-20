8f2bfa4db87e795e5824082c0e51abf34f200f69
iamnicholastom <ntompg@gmail.com>
2022-08-20 09:34:42 +0530

    minor: add responsive top navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import useStyles from './styles';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+const Navbar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton color="inherit" edge="start" style={{ outline: 'none' }} onClick={() => {}} className={classes.menuButton}>
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}} className={classes.menuButton}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button color="inherit" component={Link} to="/profile/:id" className={classes.linkButton}>
+                {!isMobile && <>My Movies</>}
+                <Avatar style={{ width: 30, height: 30 }} alt="profile" src="https://cdn5.vectorstock.com/i/1000x1000/99/94/default-avatar-placeholder-profile-icon-male-vector-23889994.jpg" />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Navbar;
+
