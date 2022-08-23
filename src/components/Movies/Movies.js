9763c1766fdb3b2b6afc3a31cf0942ef34212db2
iamnicholastom <ntompg@gmail.com>
2022-08-23 13:56:17 +0530

    patch: setup redux

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
-const Movies = () => (
-  <div>Movies</div>
-);
+const Movies = () => {
+  const { data } = useGetMoviesQuery();
+
+  console.log('+++data', data);
+
+  return (
+    <div>
+      <h1>I am a malayali</h1>
+    </div>
+  );
+};
 
 export default Movies;
---
+++ src/services/TMDB.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmbdApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    getMovies: builder.query({
+      query: () => `movie/popular?api_key=${tmdbApiKey}&page=${page}`,
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery } = tmdbApi;
